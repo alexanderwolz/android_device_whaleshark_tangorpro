e4286cae795af9e61a09c10406bdef0e8d3060ce
Alexander Wolz <mail@alexanderwolz.de>
2023-12-16 15:26:47 +0100

    Commented out PHONE_CAR_BOARD_PRODUCT

--- device-tangorpro-car.mk
+++ device-tangorpro-car.mk
@@ -5,16 +5,17 @@
 
 # This executes the following in device/google/tangorpro/tangorpro/BoardConfig.mk:
 # "include vendor/auto/embedded/products/$(PHONE_CAR_BOARD_PRODUCT)/BoardConfig.mk"
-# TODO: We need to override the path? 
-PHONE_CAR_BOARD_PRODUCT := tangorpro_car
+# TODO: We need to override the path?
+# We will leave it unassigned so it does not look up the non-existing BoardConfig
+# PHONE_CAR_BOARD_PRODUCT := tangorpro_car
 
 $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 
 $(call inherit-product, device/google/tangorpro/device-tangorpro.mk)
 
-include device/google/gs101/uwb/uwb.mk
+TARGET_SCREEN_DENSITY := 320 # from BoardConfig in raven_car, see PHONE_CAR_BOARD_PRODUCT, scaled down by 1.75x
 
-TARGET_SCREEN_DENSITY := 320 # from BoardConfig in raven_car, see PHONE_CAR_BOARD_PRODUCT
+include device/google/gs101/uwb/uwb.mk
 
 PRODUCT_PRODUCT_PROPERTIES+= \
     ro.adb.secure=0
